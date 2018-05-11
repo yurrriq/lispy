ab8bf915e6ddfea620719f53123f416ee56dff88
Eric Bailey <eric@ericb.me>
2018-05-10 20:23:37 -0500

    Add skeleton REPL

--- src/prompt.c
+++ src/prompt.c
@@ -1,4 +1,29 @@
+#include <stdio.h>
+
+#define INPUT_SIZE 2048
+
+static char input[INPUT_SIZE];
+
+char *read(const char *prompt)
+{
+    fputs(prompt, stdout);
+    return fgets(input, INPUT_SIZE, stdin);
+}
+
+void eval()
+{
+    printf("< %s", input);
+}
+
 int main(int argc, char *argv[])
 {
+    puts("Lispy v0.0.1");
+    puts("Press ctrl-c to exit\n");
+
+    while (read("> ") != NULL) {
+        eval();
+    }
+
+
     return 0;
 }
