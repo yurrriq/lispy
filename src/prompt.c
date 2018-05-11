f019dff807483843f21d67e0545183bac0994b51
Eric Bailey <eric@ericb.me>
2018-05-11 03:16:51 -0500

    Update formatting and prose

--- src/prompt.c
+++ src/prompt.c
@@ -1,7 +1,10 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <editline/readline.h>
+
+
 bool eval(char *input)
 {
     if (input && *input) {
@@ -14,15 +17,14 @@ bool eval(char *input)
     return (bool) input;
 }
 
+
 int main(int argc, char *argv[])
 {
     puts("Lispy v0.0.1");
     puts("Press ctrl-c to exit\n");
 
-    bool keep_going = true;
-    while (keep_going) {
-        keep_going = eval(readline("> "));
-    }
+    while (eval(readline("> ")))
+        continue;
 
     return 0;
 }
