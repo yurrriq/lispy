302e8a29f969d0557548aefc96733c19e8689d08
Eric Bailey <eric@ericb.me>
2018-05-13 14:09:47 -0500

    Add initial error handling

--- src/lispy.c
+++ src/lispy.c
@@ -12,37 +12,106 @@ static const char LISPY_GRAMMAR[] = {
 };
 
 
-double eval_binop(char *op, double x, double y)
+enum { LVAL_NUM, LVAL_ERR };
+
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+typedef struct {
+    int type;
+    double num;
+    int err;
+} lval;
+
+
+lval lval_num(float x)
 {
-    if (!strcmp(op, "+"))
-        return x + y;
+    lval val;
+    val.type = LVAL_NUM;
+    val.num = x;
 
-    if (!strcmp(op, "-"))
-        return x - y;
-
-    if (!strcmp(op, "*"))
-        return x * y;
-
-    if (!strcmp(op, "/"))
-        return x / y;
-
-    return 0;
+    return val;
 }
 
 
-double eval(mpc_ast_t * ast)
+lval lval_err(int err)
 {
-    if (strstr(ast->tag, "number"))
-        return atof(ast->contents);
+    lval val;
+    val.type = LVAL_ERR;
+    val.err = err;
+
+    return val;
+}
+
+
+void lval_print(lval val)
+{
+    switch (val.type) {
+    case LVAL_NUM:
+        printf("%g", val.num);
+        break;
+
+    case LVAL_ERR:
+        switch (val.err) {
+        case LERR_BAD_OP:
+            puts("Error: invalid operator");
+            break;
+        case LERR_BAD_NUM:
+            puts("Error: invalid number");
+            break;
+        case LERR_DIV_ZERO:
+            fputs("Error: division by zero", stdout);
+            break;
+        }
+        break;
+    }
+}
+
+
+void lval_println(lval val)
+{
+    lval_print(val);
+    putchar('\n');
+}
+
+
+lval eval_binop(char *op, lval x, lval y)
+{
+    if (!strcmp(op, "+"))
+        return lval_num(x.num + y.num);
+
+    if (!strcmp(op, "-"))
+        return lval_num(x.num - y.num);
+
+    if (!strcmp(op, "*"))
+        return lval_num(x.num * y.num);
+
+    if (!strcmp(op, "/")) {
+        return !y.num ? lval_err(LERR_DIV_ZERO)
+            : lval_num(x.num / y.num);
+    }
+
+    return lval_err(LERR_DIV_ZERO);
+}
+
+
+lval eval(mpc_ast_t * ast)
+{
+    if (strstr(ast->tag, "number")) {
+        errno = 0;
+        double x = strtod(ast->contents, NULL);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
+    }
 
     int i = 0;
 
     char *op = ast->children[++i]->contents;
 
-    double result = eval(ast->children[++i]);
+    lval result = eval(ast->children[++i]);
 
-    if (!strcmp(op, "-") && ast->children_num == 4)
-        return -result;
+    if (!strcmp(op, "-") && ast->children_num == 4) {
+        result.num = -result.num;
+        return result;
+    }
 
     while (++i < ast->children_num
            && strstr(ast->children[i]->tag, "expr"))
@@ -64,7 +133,7 @@ int main(int argc, char *argv[])
     mpca_lang(MPCA_LANG_DEFAULT, LISPY_GRAMMAR,
               Integer, Decimal, Number, Operator, Expr, Lispy);
 
-    puts("Lispy v0.7.0");
+    puts("Lispy v0.8.0");
     puts("Press ctrl-c to exit\n");
 
     bool nonempty;
@@ -77,8 +146,8 @@ int main(int argc, char *argv[])
             if (mpc_parse("<stdin>", input, Lispy, &parsed)) {
                 mpc_ast_t *ast = parsed.output;
 
-                double result = eval(ast);
-                printf("%g\n", result);
+                lval result = eval(ast);
+                lval_println(result);
 
                 mpc_ast_delete(ast);
             } else {
